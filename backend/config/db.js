cb5f86959f16f7ad83f08b3a2207b81b28da64c2
Krish-2806 <krish.prajapati.2806@gmail.com>
2026-07-28 13:22:15 +0530

    Updated project for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () =>{
-    mongoose.connect("mongodb://krishprajapati2806_db_user:krrish_ooo1_@ac-lwhxdbb-shard-00-00.j0cy0iu.mongodb.net:27017,ac-lwhxdbb-shard-00-01.j0cy0iu.mongodb.net:27017,ac-lwhxdbb-shard-00-02.j0cy0iu.mongodb.net:27017/?ssl=true&replicaSet=atlas-6c5iw9-shard-0&authSource=admin&appName=Cluster0")
-    .then(()=>{
-        console.log("DB Connected");
-    })
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ DB Connected");
+  } catch (error) {
+    console.error("❌ Database Connection Error:", error);
+    process.exit(1);
+  }
+};
