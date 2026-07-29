44e189b7019b2c744008be0dfa5ae02e46adeaf0
Krish-2806 <krish.prajapati.2806@gmail.com>
2026-07-29 12:19:28 +0530

    Debug SMTP connection

--- backend/utilis/sendOTP.js
+++ backend/utilis/sendOTP.js
@@ -1,55 +1,27 @@
-// import { createTransport } from "nodemailer";
-
-// const sendOTP = async (email, otp) =>{
-//     const transporter = createTransport({
-//         service: "gmail",
-//         auth: {
-//             user: process.env.EMAIL_USER,
-//             pass: process.env.EMAIL_PASS
-//         }
-//     })
-//     await transporter.sendMail({
-//         from: process.env.EMAIL_USER,
-//         to: email,
-//         subject: "Your One-Time-Password from Shelfwise Library",
-//         html: `<pre>Hello,
-
-// <strong><u>${otp}</u></strong> is your one-time password (OTP) for the Shelfwise Library.
-        
-// The code was requested from the Shelfwise Library. It will be valid for 5 minutes.
-        
-// Enjoy the website!
-
-// Shelfwise Library's Team</pre>`
-//     })
-// }
-// export default sendOTP;
-
 import { createTransport } from "nodemailer";
 
-const sendOTP = async (email, otp) => {
-  const transporter = createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const sendOTP = async (email, otp) =>{
+    const transporter = createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    })
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "Your One-Time-Password from Shelfwise Library",
+        html: `<pre>Hello,
 
-  console.log("EMAIL_USER:", process.env.EMAIL_USER);
-  console.log("EMAIL_PASS Exists:", !!process.env.EMAIL_PASS);
+<strong><u>${otp}</u></strong> is your one-time password (OTP) for the Shelfwise Library.
+        
+The code was requested from the Shelfwise Library. It will be valid for 5 minutes.
+        
+Enjoy the website!
 
-  await transporter.verify();
-  console.log("SMTP Connected Successfully");
-
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Your One-Time Password from Shelfwise Library",
-    html: `<h2>Your OTP is: ${otp}</h2>`,
-  });
-};
-
+Shelfwise Library's Team</pre>`
+    })
+}
 export default sendOTP;
+
