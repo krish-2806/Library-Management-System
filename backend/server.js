bbd000cc2d95e6a709fe6071a04069c015dee481
Krish-2806 <krish.prajapati.2806@gmail.com>
2026-07-30 12:29:49 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,8 @@ app.get("/", (req,res)=>{
     res.send("API Working");
 })
 
-app.listen(PORT, () => {
-    console.log(`Server Started on PORT ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server Started on PORT ${PORT}`);
+// });
+
+export default app;
