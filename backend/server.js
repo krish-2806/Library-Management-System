243abaef36e93a6ff70f170b23d4466c1a9e7253
Krish-2806 <krish.prajapati.2806@gmail.com>
2026-07-30 13:08:48 +0530

    edit1

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://shelfwise-library-seven.vercel.app",
+      "https://shelfwise-library-silk.vercel.app",
       "http://localhost:5173",
     ],
     credentials: true,
